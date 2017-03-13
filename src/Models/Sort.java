822107cc48a8b62825125146b059acfd2e0ae698
Eran <e.machiels@hotmail.com>
2017-03-13 15:40:34 +0100

    Moved sort interface to model package

---
+++ src/Models/Sort.java
@@ -0,0 +1,26 @@
+package Models;
+
+/**
+ * Created by jouke on 7-3-2017.
+ */
+public interface Sort {
+    /*
+     * One step further in the sorting algorithm
+     */
+    void nextStep();
+
+    /*
+     * Get the current list
+     */
+    int[] getList();
+
+    /*
+     * Get the current index
+     */
+    int getCurrentIndex();
+
+    /*
+     * @return true If list is sorted
+     */
+    boolean isSorted();
+}
